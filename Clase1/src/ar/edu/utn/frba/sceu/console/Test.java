ae72c7eab64d445a58b2fa9023797ece1810c31e
Panella <524160@CTS.COM>
2017-03-13 22:25:40 -0300

    clase1

---
+++ Clase1/src/ar/edu/utn/frba/sceu/console/Test.java
@@ -0,0 +1,44 @@
+package ar.edu.utn.frba.sceu.console;
+
+//Esta es nuestra primer clase de java
+public class Test {
+
+	// Variables
+	/*
+	 * Java es un lenguaje tipado cada variable va a tener un tipo de valor en
+	 * especial
+	 */
+
+	// cadenas ( cadenas de texto " hola mundo"
+	String nombre; // cada linea de java que cree (tiene excepciones)
+					// va a terminar con punt y coma ( ; )
+
+	// entero numeros enteros ( 1 , 2 , 3 -... )
+	int edad;
+
+	// double con punto decimal
+	double altura;
+
+	// float con punto decimal (rangos limitados)
+	float peso;
+
+	// char a un solo caracter
+	char sexo;
+
+	// boolean = guarda verdadero o falso
+	boolean fuma = true;
+
+	// Object ( comodin) aqui se puede guardar cualquier valor
+	// ALTA MENTE RECOMENDADO NO USAR!!!!!
+	Object comodin = "no usar";
+
+	/*
+	 * El metodo main es el punto de inicio de la aplicacion
+	 */
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+}
